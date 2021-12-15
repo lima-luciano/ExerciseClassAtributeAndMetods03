511fed6b2309a1e3b44e26f64b3e200a3f5be89e
Devllu <dev.limaluciano@gmail.com>
2021-12-15 20:48:28 -0300

    Add project files.

---
+++ ExerciseClassAtributeAndMetods03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciseClassAtributeAndMetods03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student();
+            
+
+            Console.Write("Student name: ");
+            s1.Name = (Console.ReadLine());
+            Console.WriteLine("Type the three student notes: ");
+            s1.Note1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            s1.Note2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            s1.Note3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine(s1);
+            
+            if (s1.Aproved())
+            {
+                Console.WriteLine("APROVED");
+            }
+            else
+            {
+                Console.WriteLine("REPROVED");
+                Console.WriteLine(s1.Remaining().ToString("F2", CultureInfo.InvariantCulture) + " POINTS MISSING");
+            }
+            
+        }
+    }
+}
---
+++ ExerciseClassAtributeAndMetods03/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciseClassAtributeAndMetods03
+{
+    class Student
+    {
+        public string Name;
+        public double Note1, Note2, Note3;
+
+
+        public double FinalNote()
+        {
+            return Note1 + Note2 + Note3;
+        }
+
+        public bool Aproved()
+        {
+            if (FinalNote() >= 60.00)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double Remaining()
+        {
+            if (Aproved())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.00 - FinalNote();
+            }
+        }
+        public override string ToString()
+        {
+            return "Final Note = " + FinalNote().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }    
+}
